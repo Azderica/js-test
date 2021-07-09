64b094224f9e67afc351a6ee88e4de763b78e580
Azderica <mh97888@gmail.com>
2021-07-09 21:56:21 +0900

    ex11 일부 수정

--- refactoring/ch6/ex11/refactoring-step-0.js
+++ refactoring/ch6/ex11/refactoring-step-0.js
@@ -1,6 +1,19 @@
 // before
-let basePrice = anOrder.basePrice
-return basePrice > 1000
+const orderDate = orderString.split(/\s+/)
+const productPrice = priceList[orderDate[0].split('-')[1]]
+const orderPrice = parseInt(orderDate[1]) * productPrice
 
 // after
-return anOrder.basePrice > 1000
+const orderRecord = parseOrder(order)
+const orderPrice = price(orderRecord, priceList)
+
+function parseOrder(aString) {
+  const values = aString.split(/\s+/)
+  return {
+    productID: values[0].split('-')[1],
+    quantity: parseInt(values[1]),
+  }
+}
+function price(order, priceList) {
+  return order.quantity * priceList[order.productID]
+}
